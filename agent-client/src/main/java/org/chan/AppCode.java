3be25c2beeac3dc1cdb78a6808279a42d5d303c6
chenzhou <luo.chen.cl@gmail.com>
2018-08-07 00:29:38 +0800

    agent

---
+++ agent-client/src/main/java/org/chan/AppCode.java
@@ -0,0 +1,20 @@
+package org.chan;
+
+public class AppCode {
+
+    public void doSomething(String something) {
+        System.out.println("do " + something);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("do completely");
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        new AppCode().doSomething("Hello World");
+        System.out.println("executeTime: " + (System.currentTimeMillis() - start));
+    }
+}
