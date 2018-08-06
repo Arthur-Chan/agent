fc69bc93c689555af53d8ac5e7533881f8d96864
chenzhou <luo.chen.cl@gmail.com>
2018-08-07 00:27:40 +0800

    agent

--- agent-client/src/main/java/org/chan/App.java
+++ agent-client/src/main/java/org/chan/App.java
@@ -1,12 +1,15 @@
 package org.chan;
 
-/**
- * Hello world!
- */
 public class App {
 
     public void doSomething(String something) {
         System.out.println("do " + something);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("do completely");
     }
 
     public static void main(String[] args) {
